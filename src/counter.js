0e80271cdbd794bf8676973a4e7d810b89bde12f
titiksha07 <titiksha0410@gmail.com>
2025-07-17 23:54:37 +0530

    string

--- src/counter.js
+++ src/counter.js
@@ -62,10 +62,27 @@ export function setupCounter() {
   //       lPointer++;
   //     }
   //     if(lPointer === rPointer) {
+  //       lPointer =0;
+  //       rPointer = 0;
   //       count++;
   //     }
   //   }
   //   return count;
   // }
   // console.log(balanced('RLRRRLLRLL'));
+
+  /* Reverse String 2 (with k and 2k steps) */
+
+//   var reverseStr = function(s, k) {
+//     s = s.split('');
+//     for(let i=0; i<s.length; i= i+(2*k)) {
+//         let mid = Math.floor(k/2);
+//         for(let j=0; j<mid; j++) {
+//             let temp = s[j+i];
+//             s[j+i] = s[k-j-1+i];
+//             s[k-j-1+i] = temp;
+//         }
+//     }
+//     return s.join('');
+// };
 }
