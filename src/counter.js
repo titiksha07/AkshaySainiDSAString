016414d2e973d59bbeed9c6de6c9b7406b615074
titiksha07 <titiksha0410@gmail.com>
2025-07-18 14:22:37 +0530

    ll

--- src/counter.js
+++ src/counter.js
@@ -118,5 +118,30 @@ export function setupCounter() {
 // }
 // console.log(longestCommonPrefix(['flower','flow','flosh']));
 
+//* Check for Valid anagram */
+
+// function anagram(s,t) {
+//   let map ={};
+//   if(s.length !== t.length) return false;
+//   for(let i=0; i<s.length; i++) {
+//     if(!map[s[i]]) {
+//       map[s[i]] = 1;
+//     } else {
+//       map[s[i]]++;
+//     }
+//   }
+  
+//   for(let i=0; i<t.length; i++) {
+//     if(!map[t[i]] || map[t[i]]<0) {
+//       return false;
+//     } else {
+//       map[t[i]]--;
+//     }
+//   }
+//   return true;
+   
+//  }
+//  console.log(anagram(['anagram','nagaram']));
+
 
 }
